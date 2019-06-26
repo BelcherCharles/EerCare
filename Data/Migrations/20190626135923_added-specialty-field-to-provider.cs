66ec895536bb58cd90a0cc45797b1949caf359e0
Charles Belcher <belchercharles@yahoo.com>
2019-06-26 10:34:13 -0400

    Mostly completed provider's controller and views

---
+++ Data/Migrations/20190626135923_added-specialty-field-to-provider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EerCare.Data.Migrations
+{
+    public partial class addedspecialtyfieldtoprovider : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderName",
+                table: "Provider",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProviderType",
+                table: "Provider",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProviderType",
+                table: "Provider");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderName",
+                table: "Provider",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- Models/Provider.cs
+++ Models/Provider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +10,12 @@ namespace EerCare.Models
     public class Provider : InheritedEntity
     {
         public int ProviderId { get; set; }
+        [Required]
+        [DisplayName("Name")]
         public string ProviderName { get; set; }
+        //[Required]
+        [DisplayName("Field")]
+        public string ProviderType { get; set; }
         
     }
 }
